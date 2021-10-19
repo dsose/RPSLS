3b5208faee6c6d83db82bb0ba35686c15e904d91
Dallas Sosebee <dallas Sosebee>
2021-10-19 17:54:59 -0500

    Add class Player One and relevant information.

---
+++ player1.js
@@ -0,0 +1,13 @@
+class PlayerOne {
+    constructor(name) {
+        this.name = name;
+        this.score = score;
+        this.gesture = ['rock' , 'paper' , 'scissors' , 'lizard' , 'spock'];
+
+
+    }
+
+
+}
+
+module.exports = PlayerOne;
